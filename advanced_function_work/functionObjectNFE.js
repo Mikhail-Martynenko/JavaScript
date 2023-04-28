300567cd2d14561bf4716574bfeb47a585459d99
Mikhail Martynenko <misha.martynenko8@yandex.ru>
2023-04-28 12:54:43 +0300

    107948 feat: fixed the block advanced_function_work

--- advanced_function_work/functionObjectNFE.js
+++ advanced_function_work/functionObjectNFE.js
@@ -9,11 +9,35 @@
 // P.S. Для того, чтобы сохранить текущее значение счётчика, можно воспользоваться как замыканием, так и свойством функции. Или сделать два варианта решения: и так, и так.
 function makeCounter() {
     let count = 0;
+
     function counter() {
         return count++;
     }
 
-    counter.set = value => count = value;
-    counter.decrease = () => count--;
+    Object.defineProperty(counter, 'get', {
+        get: function() { return count; }
+    });
+
+    Object.defineProperty(counter, "set", {
+        set: function(value) { count = value; }
+    });
+
+    Object.defineProperty(counter, "decrease", {
+        value: function() { count--; }
+    });
+
     return counter;
 }
+
+let counter = makeCounter();
+console.log(counter()); // 0
+console.log(counter()); // 1
+console.log(counter()); // 2
+
+console.log(counter.get);// 3
+counter.set = 24;
+console.log(counter.get); // 24
+counter.decrease();
+counter.decrease();
+console.log(counter.get);// 22
+
